39a5adec36ce1b1b893fb412238c790a582fb5ba
thebridge-tech <david.carvajal@thebridgeschool.es>
2020-12-09 11:50:08 +0100

    Context I (Provider)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import './App.css';
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
+import { Component } from 'react/cjs/react.development';
 
-function App() {
-  let myCompany = "Grupo ACME, S.A.";
+const AppContext = React.createContext()
+const MainPostsContext = React.createContext()
 
-  return (
-    <div className="App">
-      <Header company={myCompany} />
-      <main>
-        <h1>Hola mundo</h1>
-      </main>
+class App extends Component {
+  constructor() {
+    this.state = {
+      myCompany : "Grupo ACME, S.A."
+    }
+  }
 
-      <Footer company={myCompany} />
-    </div>
-  );
+  render() {
+    const contextData = {
+      company: this.state.myCompany,
+      otherData: 3,
+      callbackTo: (newName) => this.setState({ myCompany: newName })
+    };
+
+    return (
+      <div className="App">
+        <Header company={this.state.myCompany} />
+        
+        <AppContext.Provider value={contextData}>
+          <main>
+            <h1>Hola mundo</h1>
+
+            <MainPostsContext.Provider value="datosMain"> 
+              <MainPosts />
+            </MainPostsContext.Provider>
+
+            <SecondaryPosts />
+          </main>
+        </AppContext.Provider>
+
+        <Footer company={this.state.myCompany} />
+      </div>
+    )
+  }
 }
 
 export default App;
